2de394644b4a62f0439b80f9985ede9a2c8a45e7
Maryna Shavlak <shavlakmaryna@gmail.com>
2024-01-17 17:39:17 +0200

    add portfolio start code

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-function clearSearchBar() {
-  $('.search-input').val("");
-}
+$(document).ready(function () {
+  function clearSearchBar() {
+    $('.search-input').val('');
+  }
 
-$('.clear-btn').on("click", clearSearchBar);
+  $('.clear-btn').on('click', clearSearchBar);
+  // $('.portfolio-wrap').masonry({
+  //   itemSelector: '.portfolio-item',
+  //   // columnWidth: 200,
+  //   fitWidth: true,
+  //   // horizontalOrder: true,
+  // });
+});
