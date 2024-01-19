89909e6e59413aee386ed95fbe6a190bdbd6ecd1
Maryna Shavlak <shavlakmaryna@gmail.com>
2024-01-19 13:08:46 +0200

    add fixes

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 $(document).ready(function () {
   const achievementsSection = $('.section--achievements');
   const achievementsList = $('.achievement-value');
-
-  if (isElementInViewport(achievementsSection)) {
-    startCounterAnimation();
-  }
-
-  $(window).on('scroll', function () {
-    if (isElementInViewport(achievementsSection)) {
-      startCounterAnimation();
-      $(window).off('scroll');
-    }
-  });
+  const achievementIconsList = $('.achievement-icon');
+  const achievementTextsList = $('.achievement-text');
+  setAchievemenetSectionAnimation();
+  addEventHandlers();
+  // setClientsSlider();
 
   function isElementInViewport(element) {
     const rect = element[0].getBoundingClientRect();
@@ -33,7 +27,7 @@ $(document).ready(function () {
             Counter: $(this).text(),
           },
           {
-            duration: 5000,
+            duration: 3000,
             easing: 'swing',
             step: function (now) {
               $(this).text(Math.ceil(now));
@@ -41,17 +35,72 @@ $(document).ready(function () {
           },
         );
     });
+    achievementIconsList.animate({ scale: '1.2' }, 1000);
+    achievementTextsList.animate({ scale: '1.2' }, 1000);
   }
 
   function clearSearchBar() {
     $('.search-input').val('');
   }
 
-  $('.clear-btn').on('click', clearSearchBar);
-  // $('.portfolio-wrap').masonry({
-  //   itemSelector: '.portfolio-item',
-  //   // columnWidth: 200,
-  //   fitWidth: true,
-  //   // horizontalOrder: true,
-  // });
+  function setAchievemenetSectionAnimation() {
+    if (isElementInViewport(achievementsSection)) {
+      startCounterAnimation();
+    }
+  }
+
+  function addEventHandlers() {
+    $(window).on('scroll', function () {
+      if (isElementInViewport(achievementsSection)) {
+        startCounterAnimation();
+        $(window).off('scroll');
+      }
+    });
+    $('.clear-btn').on('click', clearSearchBar);
+  }
+
+  // function setClientsSlider() {
+  //   const totalSlides = $('.slide').length;
+  //   let currentSlide = 1;
+
+  //   function showSlide(index) {
+  //     console.log('index: ', index);
+  //     $('.slide').css('order', function (i) {
+  //       return ((index + i - 1) % totalSlides) + 1;
+  //     });
+  //     $('.indicator').removeClass('indicator--active');
+  //     $('.indicator')
+  //       .eq(index - 1)
+  //       .addClass('indicator--active');
+  //     currentSlide = index;
+  //   }
+
+  //   function nextSlide() {
+  //     currentSlide = (currentSlide % totalSlides) + 1;
+  //     showSlide(currentSlide);
+  //   }
+
+  //   function prevSlide() {
+  //     currentSlide = ((currentSlide - 2 + totalSlides) % totalSlides) + 1;
+  //     showSlide(currentSlide);
+  //   }
+
+  //   $('.indicator').on('click', function () {
+  //     showSlide($(this).index() + 1);
+  //   });
+
+  //   // setInterval(nextSlide, 1000);
+  // }
+
+  $('.owl-carousel').owlCarousel({
+    // items: 4,
+    margin: 150,
+    dots: true,
+    dotsContainer: '.indicators',
+    dotsEach: true,
+    // loop: true,
+    // autoplay: true,
+    // autoplayTimeout: 3000,
+    // autoplayHoverPause: true,
+  });
 });
